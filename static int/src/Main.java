17a63163b9c96a2ae23c6f7d70563703c9506873
noahsaa100 <67589898+noahsaa100@users.noreply.github.com>
2024-01-26 23:58:43 +0000

    Add files via upload

---
+++ static int/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Friend friend1 = new Friend("Aasiyah");
+        Friend friend2 = new Friend("James");
+
+        Friend.displayFriends();
+
+    }
+}
