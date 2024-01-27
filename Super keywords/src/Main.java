75f8b9739289d59a435b011baecd9cd9265abed6
noahsaa100 <67589898+noahsaa100@users.noreply.github.com>
2024-01-27 13:40:37 +0000

    Super keywords

---
+++ Super keywords/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Hero hero1 = new Hero("Batman", 30, "Money");
+        Hero hero2 = new Hero("Superman", 40, "Everything");
+        System.out.println(hero2.toString());
+
+    }
+}
